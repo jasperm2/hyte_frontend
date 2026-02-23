5a2523c29933d53ba0b6e3292d0024d4a5bc6b2e
Jasper Mustasilta <zipuli69@gmail.com>
2026-02-23 06:33:25 +0200

    commit

---
+++ vite.config.js
@@ -0,0 +1,20 @@
+import { dirname, resolve } from 'node:path';
+import { fileURLToPath } from 'node:url';
+import { defineConfig } from 'vite';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        home: resolve(__dirname, 'index.html'),
+        bmi: resolve(__dirname, 'bmi.html'),
+        viikoteh: resolve(__dirname, 'viikkotehtavat.html'),
+      },
+    },
+  },
+  // Public base path could be set here too:
+  //base: '/~ullamu/hyte/',
+  base: './',
+});
